5be3ac75b9d03b3c732217b0637557a22ea03c9c
LPM <43784584+mirloupa@users.noreply.github.com>
2020-10-13 17:33:11 +0100

    update

--- Effectors/XfShiny/app.R
+++ Effectors/XfShiny/app.R
@@ -1,9 +1,11 @@
-# 01-kmeans-app
+# xf-app
 
 palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
   "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
 library(shiny)
+
+# USER INTERFACE
 
 ui <- fluidPage(
   headerPanel('Iris k-means clustering'),
@@ -19,6 +21,7 @@ ui <- fluidPage(
   )
 )
 
+# SERVER
 server <- function(input, output) {
 
   selectedData <- reactive({
